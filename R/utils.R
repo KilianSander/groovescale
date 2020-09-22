fabc9e75ce8b48bc1937172feef8db63e00f52da
KilianSander <kilian.sander@aol.com>
2020-09-22 17:18:08 +0200

    adding some functions (cf psychTestR utils)

---
+++ R/utils.R
@@ -0,0 +1,23 @@
+is.scalar <- function(x) {
+  identical(length(x), 1L)
+}
+
+is.scalar.character <- function(x) {
+  is.character(x) && is.scalar(x)
+}
+
+is.scalar.numeric <- function(x) {
+  is.numeric(x) && is.scalar(x)
+}
+
+is.scalar.logical <- function(x) {
+  is.logical(x) && is.scalar(x)
+}
+
+tagify <- function(x) {
+  stopifnot(is.character(x) || is(x, "shiny.tag"))
+  if (is.character(x)) {
+    stopifnot(is.scalar(x))
+    shiny::p(x)
+  } else x
+}
