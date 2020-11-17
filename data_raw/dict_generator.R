83465278b70bfef6a5386442a8ab8a533a5a68f9
KilianSander <kilian.sander@aol.com>
2020-11-17 11:35:17 +0100

    updated dict_generator

--- data_raw/dict_generator.R
+++ data_raw/dict_generator.R
@@ -1,4 +1,4 @@
-raw_dict <- read.csv(fileEncoding = "UTF8", file = "./data_raw/groovescale_dict.csv")
+raw_dict <- read.csv(fileEncoding = "UTF8", file = "./data_raw/groovescale_dict.csv", stringsAsFactors = FALSE, header = TRUE)
 
 groovescale_dict <- psychTestR::i18n_dict$new(raw_dict)
 
