14c1bb7a452d1eaad10489ec6188b5cf7add3a26
KilianSander <kilian.sander@aol.com>
2021-03-16 17:59:05 +0100

    Added multi radiobutton audio page

--- R/test_elements.R
+++ R/test_elements.R
@@ -45,6 +45,7 @@ multi_radiobutton_NAFC_page <- function(label,
                                         failed_validation_message = "Answer missing!",
                                         save_answer = TRUE,
                                         hide_response_ui = FALSE,
+                                        random_order = FALSE,
                                         response_ui_id = "response_ui",
                                         on_complete = NULL,
                                         admin_ui = NULL) {
@@ -69,16 +70,22 @@ multi_radiobutton_NAFC_page <- function(label,
                                                        labels = labels,
                                                        trigger_button_text = trigger_button_text,
                                                        hide = hide_response_ui,
+                                                       random_order = random_order,
                                                        id = response_ui_id)
   )
-  get_answer <- function(input, ...)
-    input[[label]]
-  validate <- function(answer, ...)
-    if (!is.null(answer)) {
+  itemlist <- 1:length(prompts)
+  names(itemlist) <- lapply(1:length(prompts), function(i) paste0("item",i))
+  get_answer <- function(input, ...) {
+    sapply(1:length(prompts), function(i) input[[paste0("item",i)]], simplify = TRUE, USE.NAMES = TRUE)
+  }
+
+  validate <- function(answer, ...) {
+    if (sum(unlist(lapply(1:length(prompts), function(i) !is.null(answer[[i]])))) == length(prompts)) {
       TRUE
     } else {
       failed_validation_message
     }
+  }
   psychTestR::page(ui = ui,
                    label = label,
                    get_answer = get_answer,
@@ -87,8 +94,9 @@ multi_radiobutton_NAFC_page <- function(label,
                    on_complete = on_complete,
                    final = FALSE,
                    admin_ui = admin_ui
-                   )
+  )
 }
+
 #' Make multiple NAFC radiobuttons with the same choices
 #'
 #' Creates the HTML code for multiple n-alternative forced choice response
@@ -112,7 +120,11 @@ multi_radiobutton_NAFC_page <- function(label,
 #'
 #' @param trigger_button_text (Character scalar) Text for the trigger button.
 #'
-#' @param hide (Boolean scalar) Whether the radiobuttons should be hidden (possibly to be shown later).
+#' @param hide (Boolean scalar) Whether the radiobuttons should be hidden
+#' (possibly to be shown later).
+#'
+#' @param random_order (Boolean scalar) Whether the order of the items should
+#' be randomized.
 #'
 #' @param id (Character scalar) HTML ID for the div containing the radiobuttons.
 #'
@@ -123,10 +135,12 @@ make_ui_multi_radiobutton_NAFC <- function(label,
                                            labels = NULL,
                                            trigger_button_text = "Continue",
                                            hide = FALSE,
+                                           random_order = FALSE,
                                            id = "response_ui") {
   stopifnot(
     is.character.or.numeric(choices) && length(choices) > 0L,
     is.scalar.logical(hide),
+    is.scalar.logical(random_order),
     is.null(labels) ||
       ((is.character(labels) || is.list(labels)) &&
          length(labels) == length(choices)
@@ -142,24 +156,118 @@ make_ui_multi_radiobutton_NAFC <- function(label,
     purrr::map(labels, function(label)
       shiny::tags$span(style = "font-size: 15px; line-height: 15px;", label))
 
-  multi_radiobuttons <-
-    do.call(shiny::tagList,
-            lapply(1:length(prompts), function(i) {
-              shiny::tags$div(style = "text-align: left;",
-                              shiny::tags$b(prompts[i]),
-                              shiny::radioButtons(paste0("item",i),
-                                                  "",
-                                                  choiceNames = labels,
-                                                  choiceValues = choices,
-                                                  inline = TRUE,
-                                                  selected = 0))}
-            )
-    )
-  multi_radiobuttons <- htmltools::renderTags(multi_radiobuttons)
-  multi_radiobuttons_div <- shiny::HTML(multi_radiobuttons$html)
+  order <- if(random_order) {sample(1:length(prompts))} else 1:length(prompts)
+
+  multi_radiobuttons_div <- shiny::tags$div(
+    mapply(function(prompt_number) {
+      shiny::tags$div(style = "text-align: left;",
+                      shiny::tags$b(prompts[prompt_number]),
+                      shiny::radioButtons(inputId = paste0("item", prompt_number),
+                                          label = "",
+                                          choiceNames = labels,
+                                          choiceValues = choices,
+                                          inline = TRUE,
+                                          selected = character(0)))
+    }, order, SIMPLIFY = FALSE, USE.NAMES = FALSE)
+  )
+
   shiny::tags$div(id = id,
                   style = "inline-block;",
                   multi_radiobuttons_div,
                   psychTestR::trigger_button("next", trigger_button_text)
-                  )
+  )
+}
+
+media.js <- list(
+  media_not_played = "var media_played = false;",
+  media_played = "media_played = true;",
+  play_media = "document.getElementById('media').play();",
+  show_media_btn = paste0("if (!media_played) ",
+                          "{document.getElementById('btn_play_media')",
+                          ".style.visibility='inherit'};"),
+  hide_media_btn = paste0("document.getElementById('btn_play_media')",
+                          ".style.visibility='hidden';"),
+  show_responses = "document.getElementById('response_ui').style.visibility = 'inherit';"
+)
+
+media_mobile_play_button <- function(btn_play_prompt) shiny::tags$p(
+  shiny::tags$strong(btn_play_prompt,
+                     id = "btn_play_media",
+                     style = "visibility: hidden",
+                     onclick = media.js$play_media))
+
+#' Make multiple radiobutton NAFC audio page
+#'
+#' Creates a multiple radiobutton n-alternative forced choice page for a single audio file.
+#'
+#' @param url URL to the audio. Can be an absolute URL (e.g.
+#' "http://mysite.com/audio.mp3") or a URL relative to the /www directory (e.g.
+#' "audio.mp3").
+#' @param type Audio type (e.g. 'mp3'). Defaults to the provided file extension.
+#' @param show_controls Whether or not to show audio controls to the
+#' participant, so that they can control audio playback.
+#' @param allow_download Whether the participant is given a button to download
+#' the audio file; only relevant if \code{show_controls} is \code{TRUE}.
+#' @param wait Whether to wait for the audio to finish before displaying the
+#' response buttons.
+#' @param loop Whether the audio should loop.
+#'
+#' @inherit multi_radiobutton_NAFC_page
+#' @inherit psychTestR::page
+#' @inherit make_ui_multi_radiobutton_NAFC
+#'
+#' @export
+audio_multi_radiobutton_NAFC_page <- function(label,
+                                              prompts,
+                                              choices,
+                                              url,
+                                              instruction = "",
+                                              labels = NULL,
+                                              type = tools::file_ext(url),
+                                              trigger_button_text = "Continue",
+                                              failed_validation_message = "Answer missing!",
+                                              save_answer = TRUE,
+                                              hide_response_ui = FALSE,
+                                              random_order = FALSE,
+                                              response_ui_id = "response_ui",
+                                              on_complete = NULL,
+                                              wait = TRUE,
+                                              loop = FALSE,
+                                              admin_ui = NULL,
+                                              btn_play_prompt = if (!show_controls) "Click here to play",
+                                              show_controls = FALSE,
+                                              allow_download = FALSE) {
+  stopifnot(is.scalar.character(label),
+            is.character.or.numeric(choices),
+            is.scalar.character(url),
+            is.scalar.logical(wait),
+            is.scalar.logical(loop),
+            is.scalar.logical(show_controls),
+            is.scalar.logical(allow_download),
+            is.scalar.logical(hide_response_ui))
+
+  audio_ui <- shiny::tags$div(shiny::tags$audio(
+    shiny::tags$head(shiny::tags$script(shiny::HTML(media.js$media_not_played))),
+    shiny::tags$source(src = url, type = paste0("audio/", type)),
+    id = "media",
+    preload = "auto",
+    autoplay = "autoplay",
+    loop = if (loop) "loop",
+    oncanplaythrough = media.js$show_media_btn,
+    onplay = paste0(media.js$media_played, media.js$hide_media_btn),
+    onended = if (wait) media.js$show_responses else "null",
+    controls = if (show_controls) "controls",
+    controlsList = if (!allow_download) "nodownload"
+  ), media_mobile_play_button(btn_play_prompt))
+  instruction2 <- shiny::tags$div(tagify(instruction), audio_ui)
+  multi_radiobutton_NAFC_page(label = label, prompts = prompts, choices = choices, labels = labels,
+                              trigger_button_text = trigger_button_text,
+                              failed_validation_message = failed_validation_message,
+                              save_answer = save_answer,
+                              hide_response_ui = hide_response_ui,
+                              random_order = random_order,
+                              response_ui_id = response_ui_id,
+                              on_complete = on_complete,
+                              admin_ui = admin_ui,
+                              instruction = instruction2)
 }
